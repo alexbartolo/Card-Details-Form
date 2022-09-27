b44bfccd16170c0bad26e580fdc7747894da4bba
Alex Bartolo <alexj.bartolo@gmail.com>
2022-09-27 15:49:49 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import "./styles.css"
+import CardApp from "./CardApp"
+
+createRoot(document.getElementById("root")).render(<CardApp />)
